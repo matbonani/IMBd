42d4c24a219ba998ecc8da51e55e4e5e3a9f9499
Matheus Bonani <bonani.matheus@gmail.com>
2022-05-24 14:43:19 -0300

    test streamplatform

--- watch_list/tests.py
+++ watch_list/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+from watch_list.api import serializers
+from watch_list import models
+
+
+class StreamPlatformTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='example', password='bonani123456')  # normal user
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects\
+            .create(name='example', about='just a stream', website='http://www.example.com')
+
+    def test_streamplatform_create(self):
+        data = {
+            "name": "example",
+            "about": "just a streamplatform",
+            "webisite": "https://netflix.com"
+        }
+        response = self.client.post(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_streamplatform_list(self):
+        response = self.client.get(reverse('streamplatform-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_streamplatform_detail(self):
+        response = self.client.get(reverse('streamplatform-detail', args=(self.stream.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_streamplatform_delete(self):
+        response = self.client.delete(reverse('streamplatform-detail', args=(self.stream.id,)))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_streamplatform_update(self):
+        data = {
+            "name": "examplePUT",
+            "about": "just a streamplatformPUT",
+            "webisite": "https://netflix.com"
+        }
+        response = self.client.put(reverse('streamplatform-detail', args=(self.stream.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
